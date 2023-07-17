7685020b0cbbd70bb30b11806d1974b2da799394
lwz <905328134@qq.com>
2023-07-17 22:38:10 +0800

    initial

--- scripts/cifar100-semi/byol.sh
+++ scripts/cifar100-semi/byol.sh
@@ -1,4 +1,4 @@
-conda activate torch
+#conda activate torch
 python main_continual_semi.py \
       --num_tasks 5 \
       --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
