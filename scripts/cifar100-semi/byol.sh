e61e558bf9a02b15ea40e4f3060a3a680be70cec
lwz <905328134@qq.com>
2023-07-17 22:56:46 +0800

    initial

--- scripts/cifar100-semi/byol.sh
+++ scripts/cifar100-semi/byol.sh
@@ -1,10 +1,13 @@
 #conda activate torch
-python main_continual_semi.py \
-      --num_tasks 5 \
-      --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-      --pretrained_method byol \
-      --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
-      --project semi-IPC
+for lambda in 2.0 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.05 0.025 0.01; do
+  python main_continual_semi.py \
+    --num_tasks 5 \
+    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+    --pretrained_method byol \
+    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+    --pl_lambda $lambda \
+    --project semi-IPC
+done
 #--pretrained_model /share/wenzhuoliu/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
 #python main_continual_semi.py \
 #      --num_tasks 10 \
