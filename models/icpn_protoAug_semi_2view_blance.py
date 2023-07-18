e7c13284ddb86bea1e8f8e47e1cb75296787d3e4
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-07-19 02:29:24 +0800

    initial

--- models/icpn_protoAug_semi_2view_blance.py
+++ models/icpn_protoAug_semi_2view_blance.py
@@ -108,7 +108,7 @@ class IncrementalCPN(pl.LightningModule):
         x_unlabel_high_conf = x_unlabel[mask]
         target_unlabel_high_conf = max_logits_unlabel[mask]
         target_unlabel_high_conf_2 = targets_unlabel[mask]
-        target_unlabel_high_conf = targets_unlabel[mask]
+        # target_unlabel_high_conf = targets_unlabel[mask]
 
         # import pdb;pdb.set_trace()
 
