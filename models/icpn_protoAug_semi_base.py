86787ee5835fb87af2f51bd3257c9c2596e085ed
lwz <905328134@qq.com>
2023-07-18 20:18:53 +0800

    initial

--- models/icpn_protoAug_semi_base.py
+++ models/icpn_protoAug_semi_base.py
@@ -91,6 +91,22 @@ class IncrementalCPN(pl.LightningModule):
         # loss = ce_loss + pl_loss * self.pl_lambda + protoAug_loss * self.protoAug_lambda
         loss = ce_loss + pl_loss * self.pl_lambda
 
+
+        # unlabel data
+        x_unlabel, _ = batch['unsupervised_loader']
+        logits_unlabel = -1.*self(x_unlabel)
+        _, max_indices = torch.max(logits_unlabel, dim=1)
+        mask = logits_unlabel[torch.arange(logits_unlabel.shape[0]), max_indices] > 0.75
+        x_unlabel_high_conf = x_unlabel[mask]
+        predictions_high_conf = max_indices[mask]
+        semi_x_all = torch.cat([x, x_unlabel_high_conf])
+        semi_target_all = torch.cat([targets, predictions_high_conf])
+        semi_loss = F.cross_entropy(-1.*self(semi_x_all), semi_target_all)
+
+
+
+
+
         out = {"ce_loss": ce_loss, "pl_loss": pl_loss, 'protoAug_loss': protoAug_loss, "acc": acc, "loss": loss}
         log_dict = {"train_" + k: v for k, v in out.items()}
         self.log_dict(log_dict, on_epoch=True, sync_dist=True)
