3fd68a1aed97ed6654a7313f4c2f0851b666dce1
lwz <905328134@qq.com>
2023-07-18 21:04:04 +0800

    initial

--- models/icpn_protoAug_semi_base.py
+++ models/icpn_protoAug_semi_base.py
@@ -100,7 +100,7 @@ class IncrementalCPN(pl.LightningModule):
         # mask = logits_unlabel[torch.arange(logits_unlabel.shape[0]), max_logits_unlabel] > 0.75
         mask = probabilities_unlabel[torch.arange(probabilities_unlabel.shape[0]), max_probabilities_unlabel] > 0.75
 
-        import pdb;pdb.set_trace()
+        # import pdb;pdb.set_trace()
 
         x_unlabel_high_conf = x_unlabel[mask]
         target_unlabel_high_conf = max_logits_unlabel[mask]
