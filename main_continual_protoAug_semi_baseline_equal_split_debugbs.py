e3774a51d7cb31016c18df2dc9f3edda34ff49fa
lwz <905328134@qq.com>
2023-07-18 22:43:39 +0800

    initial

--- main_continual_protoAug_semi_baseline_equal_split_debugbs.py
+++ main_continual_protoAug_semi_baseline_equal_split_debugbs.py
@@ -96,7 +96,7 @@ def main():
         test_loader = DataLoader(test_dataset_task, batch_size=64, shuffle=True)
 
         supervised_data = keep_n_samples_per_class(train_dataset_task, n=500)
-        supervised_loader = DataLoader(supervised_data, batch_size=8, shuffle=True)
+        supervised_loader = DataLoader(supervised_data, batch_size=64, shuffle=True)
 
         train_loaders = {
             "unsupervised_loader": train_loader,
