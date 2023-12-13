9bd82e08fe56494c68dee6f4196cd88ed4726400
lwz <905328134@qq.com>
2023-12-13 23:31:59 +0800

    initial

--- main_continual_protoAug_semi_2view_equal_split_real_remove_fixfeature_debugbs_blance_dualloss_fixbug_imagenet100.py
+++ main_continual_protoAug_semi_2view_equal_split_real_remove_fixfeature_debugbs_blance_dualloss_fixbug_imagenet100.py
@@ -158,7 +158,7 @@ def main():
         # train_loader = DataLoader(train_dataset_task, batch_size=64, shuffle=True)
         # test_loader = DataLoader(test_dataset_task, batch_size=64, shuffle=True)
 
-        train_loader = DataLoader(train_dataset_task, batch_size=256, shuffle=True,pin_memory=False)
+        train_loader = DataLoader(train_dataset_task, batch_size=256, shuffle=True,pin_memory=True)
         dual_loader = DataLoader(dual_dataset_task, batch_size=256, shuffle=True,pin_memory=True,num_workers=16)
         test_loader = DataLoader(test_dataset_task, batch_size=64, shuffle=True,pin_memory=True,num_workers=8)
 
