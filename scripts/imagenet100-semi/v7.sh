42c502153ae6620d8432f89fc63bb2cd07c5c6a1
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-12-16 02:04:36 +0800

    initial

--- scripts/imagenet100-semi/v7.sh
+++ scripts/imagenet100-semi/v7.sh
@@ -10,7 +10,7 @@
 #done
 
 CUDA_VISIBLE_DEVICES=3 python main_imagenet100_v7.py \
-  --num_tasks 5 \
+  --num_tasks 2 \
   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
   --pretrained_method byol \
   --dataset imagenet100 \
