50c31bbf0319f59c85529e9028e0fe87ca651781
lwz <905328134@qq.com>
2023-12-17 01:28:03 +0800

    initial

--- main_cub200_v4.py
+++ main_cub200_v4.py
@@ -170,8 +170,8 @@ def main():
         # train_loader = DataLoader(train_dataset_task, batch_size=64, shuffle=True)
         # test_loader = DataLoader(test_dataset_task, batch_size=64, shuffle=True)
 
-        supervised_data = keep_n_samples_per_class(train_dataset_task, n=5)
-        supervised_data_std = keep_n_samples_per_class(train_dataset_task_std, n=5)
+        supervised_data = keep_n_samples_per_class(train_dataset_task, n=10)
+        supervised_data_std = keep_n_samples_per_class(train_dataset_task_std, n=10)
         cpn_means = compute_class_means(supervised_data_std, encoder, batch_size=512)
         train_loader = DataLoader(train_dataset_task, batch_size=64, shuffle=True, pin_memory=True, num_workers=2)
         dual_loader = DataLoader(dual_dataset_task, batch_size=64, shuffle=True, pin_memory=True, num_workers=2)
