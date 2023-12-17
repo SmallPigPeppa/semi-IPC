61fbd7e5554afb36790cb4689abbe5d3bc2d1f9b
lwz <905328134@qq.com>
2023-12-17 19:28:26 +0800

    initial

--- scripts/imagenet100-semi/v3_b50.sh
+++ scripts/imagenet100-semi/v3_b50.sh
@@ -23,15 +23,15 @@ CUDA_VISIBLE_DEVICES=1 python main_imagenet100_v3_b50.py \
   --cpn_initial means
 
 
-CUDA_VISIBLE_DEVICES=1 python main_imagenet100_v3_b50.py \
-  --num_tasks 11 \
-  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
-  --pretrained_method byol \
-  --dataset imagenet100 \
-  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
-  --pl_lambda 0.2 \
-  --project semi-IPC-debug-v9 \
-  --epochs 50 \
-  --perfix v3-b50-semi-dual- \
-  --cpn_initial means
+#CUDA_VISIBLE_DEVICES=1 python main_imagenet100_v3_b50.py \
+#  --num_tasks 11 \
+#  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+#  --pretrained_method byol \
+#  --dataset imagenet100 \
+#  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+#  --pl_lambda 0.2 \
+#  --project semi-IPC-debug-v9 \
+#  --epochs 50 \
+#  --perfix v3-b50-semi-dual- \
+#  --cpn_initial means
 
