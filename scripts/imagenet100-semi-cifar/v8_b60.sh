98a3fd1b07919b0158e46a95d01933f0a409fcd1
lwz <905328134@qq.com>
2023-12-18 15:20:41 +0800

    initial

---
+++ scripts/imagenet100-semi-cifar/v8_b60.sh
@@ -0,0 +1,29 @@
+#conda activate torch
+#for lambda in 2.0 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.05 0.025 0.01; do
+#  python main_continual_protoAug.py \
+#    --num_tasks 5 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#    --pl_lambda $lambda \
+#    --project semi-IPC
+#done
+
+CUDA_VISIBLE_DEVICES=4 python main_imagenet100_v8.py \
+  --num_tasks 9 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset cifar100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+  --pl_lambda 0.2 \
+  --project semi-IPC-debug-v9 \
+  --epochs 50 \
+  --perfix b60-v8-semi-dual- \
+  --cpn_initial means
+
+
+
+
+
+
+
