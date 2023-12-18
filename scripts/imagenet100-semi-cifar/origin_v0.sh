212b63cf01a2be56d0581f43d1900ae3fef34be6
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-12-19 02:11:08 +0800

    initial

---
+++ scripts/imagenet100-semi-cifar/origin_v0.sh
@@ -0,0 +1,29 @@
+#conda activate torch
+#for lambda in 2.0 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.05 0.025 0.01; do
+#  python main_continual_protoAug.py \
+#    --num_tasks 5 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#    --pl_lambda $lambda \
+#    --project semi-IPC
+#done
+
+CUDA_VISIBLE_DEVICES=0 python main_cifar100_origin_v0.py \
+  --num_tasks 10 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset cifar100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+  --pl_lambda 0.2 \
+  --project semi-IPC-debug-v9-cifar \
+  --epochs 50 \
+  --perfix origin-v0- \
+  --cpn_initial means
+
+
+
+
+
+
+
