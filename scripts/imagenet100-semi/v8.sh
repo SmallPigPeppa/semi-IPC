c75bc1b41dd473e0822f7d64b629d940f928da78
lwz <905328134@qq.com>
2023-12-18 15:38:38 +0800

    initial

--- scripts/imagenet100-semi/v8.sh
+++ scripts/imagenet100-semi/v8.sh
@@ -9,8 +9,8 @@
 #    --project semi-IPC
 #done
 
-CUDA_VISIBLE_DEVICES=2 python main_imagenet100_v8.py \
-  --num_tasks 10 \
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8.py \
+  --num_tasks 5 \
   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
   --pretrained_method byol \
   --dataset imagenet100 \
