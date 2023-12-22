bad4a463b2c06ddc6e8dfd9023c8d0cd3a9d6b53
lwz <905328134@qq.com>
2023-12-22 21:36:27 +0800

    initial

--- scripts/mini-semi/v8_5samples.sh
+++ scripts/mini-semi/v8_5samples.sh
@@ -9,6 +9,44 @@
 #    --project semi-IPC
 #done
 
+
+
+for lambda in 0.5 0.2 0.8; do
+#  python main_continual_protoAug.py \
+#    --num_tasks 5 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#    --pl_lambda $lambda \
+#    --project semi-IPC
+
+    CUDA_VISIBLE_DEVICES=1 python main_mini_v8_s5.py \
+    --num_tasks 5 \
+    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/solo-learn-latest/trained_models/byol/frxj6kgh/byol-imagenet-mini-frxj6kgh-ep=999.ckpt \
+    --pretrained_method byol \
+    --dataset mini \
+    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/miniImageNet \
+    --pl_lambda $lambda \
+    --project semi-IPC-mini \
+    --epochs 50 \
+    --perfix s5-v8- \
+    --cpn_initial means
+
+
+  CUDA_VISIBLE_DEVICES=1 python main_mini_v8_s5.py \
+    --num_tasks 10 \
+    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/solo-learn-latest/trained_models/byol/frxj6kgh/byol-imagenet-mini-frxj6kgh-ep=999.ckpt \
+    --pretrained_method byol \
+    --dataset mini \
+    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/miniImageNet \
+    --pl_lambda $lambda \
+    --project semi-IPC-mini \
+    --epochs 50 \
+    --perfix s5-v8- \
+    --cpn_initial means
+done
+
+
 CUDA_VISIBLE_DEVICES=1 python main_mini_v8_s5.py \
   --num_tasks 5 \
   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/solo-learn-latest/trained_models/byol/frxj6kgh/byol-imagenet-mini-frxj6kgh-ep=999.ckpt \
