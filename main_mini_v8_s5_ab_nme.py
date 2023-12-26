cd86b8a352c8ed8f61ff81837db8b37099577b75
lwz <905328134@qq.com>
2023-12-27 00:30:56 +0800

    initial

--- main_mini_v8_s5_ab_nme.py
+++ main_mini_v8_s5_ab_nme.py
@@ -190,7 +190,7 @@ def main():
         model.encoder = encoder
         model.protoAug_start()
         # trainer.fit(model, train_loaders, test_loader)
-        trainer.val(model,  test_loader)
+        trainer.validate(model,  test_loader)
         wandb.finish()
         model.protoAug_end()
 
