5a54801bad17535d9c837058176869000de8bffa
lwz <905328134@qq.com>
2023-12-26 22:18:24 +0800

    initial

--- scripts/imagenet100-semi-cifar/origin_v8_5sample_ab.sh
+++ scripts/imagenet100-semi-cifar/origin_v8_5sample_ab.sh
@@ -10,30 +10,30 @@
 #done
 
 
-# +pl + dual + pa
-CUDA_VISIBLE_DEVICES=0 python main_cifar100_origin_v8_s5_ab.py \
-  --num_tasks 10 \
-  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-  --pretrained_method byol \
-  --dataset cifar100 \
-  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
-  --pl_lambda 0.02 \
-  --project semi-IPC-ab \
-  --epochs 50 \
-  --perfix s5-v8- \
-  --cpn_initial means
-
-CUDA_VISIBLE_DEVICES=0 python main_cifar100_origin_v8_s5_ab.py \
-  --num_tasks 5 \
-  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-  --pretrained_method byol \
-  --dataset cifar100 \
-  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
-  --pl_lambda 0.02 \
-  --project semi-IPC-ab \
-  --epochs 50 \
-  --perfix s5-v8- \
-  --cpn_initial means
+## +pl + dual + pa
+#CUDA_VISIBLE_DEVICES=0 python main_cifar100_origin_v8_s5_ab.py \
+#  --num_tasks 10 \
+#  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#  --pretrained_method byol \
+#  --dataset cifar100 \
+#  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#  --pl_lambda 0.02 \
+#  --project semi-IPC-ab \
+#  --epochs 50 \
+#  --perfix s5-v8- \
+#  --cpn_initial means
+#
+#CUDA_VISIBLE_DEVICES=0 python main_cifar100_origin_v8_s5_ab.py \
+#  --num_tasks 5 \
+#  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#  --pretrained_method byol \
+#  --dataset cifar100 \
+#  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#  --pl_lambda 0.02 \
+#  --project semi-IPC-ab \
+#  --epochs 50 \
+#  --perfix s5-v8- \
+#  --cpn_initial means
 
 # +pl + dual
 CUDA_VISIBLE_DEVICES=0 python main_cifar100_origin_v8_s5_ab.py \
