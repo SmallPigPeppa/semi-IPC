e9fadb9c8b798512b112f809736e9de108b396db
lwz <905328134@qq.com>
2023-12-26 22:52:35 +0800

    initial

--- scripts/imagenet100-semi/v8_ab.sh
+++ scripts/imagenet100-semi/v8_ab.sh
@@ -1,24 +1,118 @@
-#conda activate torch
-#for lambda in 2.0 1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.05 0.025 0.01; do
-#  python main_continual_protoAug.py \
-#    --num_tasks 5 \
-#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-#    --pretrained_method byol \
-#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
-#    --pl_lambda $lambda \
-#    --project semi-IPC
-#done
 
-CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8.py \
+
+# +pl + dual + pa
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 10 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
+  --cpn_initial means
+
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
   --num_tasks 5 \
   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
   --pretrained_method byol \
   --dataset imagenet100 \
-  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
   --pl_lambda 0.2 \
-  --project semi-IPC-debug-v9 \
+  --project semi-IPC-ab \
   --epochs 50 \
-  --perfix v8-semi-dual- \
+  --perfix v8- \
+  --cpn_initial means
+
+# +pl + dual
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 10 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --pa_lambda 0. \
+  --dual_lambda 1.0 \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
+  --cpn_initial means
+
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 5 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --pa_lambda 0. \
+  --dual_lambda 1.0 \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
   --cpn_initial means
 
 
+
+
+
+
+# +pl
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 10 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --pa_lambda 0. \
+  --dual_lambda 0. \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
+  --cpn_initial means
+
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 5 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --pa_lambda 0. \
+  --dual_lambda 0. \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
+  --cpn_initial means
+
+# +None
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 10 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0. \
+  --pa_lambda 0. \
+  --dual_lambda 0. \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
+  --cpn_initial means
+
+CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_ab.py \
+  --num_tasks 5 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path  /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0. \
+  --pa_lambda 0. \
+  --dual_lambda 0. \
+  --project semi-IPC-ab \
+  --epochs 50 \
+  --perfix v8- \
+  --cpn_initial means
