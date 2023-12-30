f3dd4d142572ad86a77dd9b00879e8712e65d8e5
lwz <905328134@qq.com>
2023-12-31 00:22:39 +0800

    initial

---
+++ scripts/ab/run_addall.sh
@@ -0,0 +1,7 @@
+bash scripts/ab/run_add2.sh &
+bash scripts/mini-semi/v8_5samples_ablambda.sh &
+bash scripts/imagenet100-semi-cifar/origin_v8_5sample_ablamda.sh &
+bash scripts/imagenet100-semi/v8_ablambda.sh &
+bash scripts/imagenet100-semi/v8_abnum.sh &
+bash scripts/imagenet100-semi-cifar/origin_v8_5sample_abnum.sh &
+bash scripts/mini-semi/v8_5samples_abnum.sh
