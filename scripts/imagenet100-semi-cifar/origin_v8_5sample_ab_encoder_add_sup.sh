896a82554afe964a8c8a602eb225524bcf701e58
lwz <905328134@qq.com>
2023-12-31 23:52:24 +0800

    initial

---
+++ scripts/imagenet100-semi-cifar/origin_v8_5sample_ab_encoder_add_sup.sh
@@ -0,0 +1,29 @@
+
+
+CUDA_VISIBLE_DEVICES=3 python main_cifar100_origin_v8_s5_ab.py \
+  --num_tasks 5 \
+   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/semi-IPC-v9/sup/checkpoints/resnet50-cifar/test.ckpt \
+  --pretrained_method sup \
+  --dataset cifar100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+  --pl_lambda 0.02 \
+  --project semi-IPC-ab-encoder \
+  --epochs 50 \
+  --perfix s5-v8- \
+  --cpn_initial means &
+
+
+CUDA_VISIBLE_DEVICES=3 python main_cifar100_origin_v8_s5_ab.py \
+  --num_tasks 10 \
+   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/semi-IPC-v9/sup/checkpoints/resnet50-cifar/test.ckpt \
+  --pretrained_method sup \
+  --dataset cifar100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+  --pl_lambda 0.02 \
+  --project semi-IPC-ab-encoder \
+  --epochs 50 \
+  --perfix s5-v8- \
+  --cpn_initial means
+
+
+
