7ec3fd2fa46a8398075a156aae7f8ce5dac97a75
lwz <905328134@qq.com>
2024-01-05 00:37:03 +0800

    initial

--- scripts/imagenet100-semi/v8_abnum.sh
+++ scripts/imagenet100-semi/v8_abnum.sh
@@ -1,5 +1,36 @@
+#
+#for lambda in 1 3 5 20 50 100 200; do
+#
+#  CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_abnum.py \
+#    --num_tasks 10 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --dataset imagenet100 \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+#    --pl_lambda 0.2 \
+#    --project semi-IPC-abnum \
+#    --epochs 50 \
+#    --num_sample $lambda \
+#    --perfix $lambda- \
+#    --cpn_initial means &
+#
+#
+#  CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_abnum.py \
+#    --num_tasks 5 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --dataset imagenet100 \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+#    --pl_lambda 0.2 \
+#    --project semi-IPC-abnum \
+#    --epochs 50 \
+#    --num_sample $lambda \
+#    --perfix $lambda- \
+#    --cpn_initial means
+#
+#done
 
-for lambda in 1 3 5 20 50 100 200; do
+for lambda in 1300; do
 
   CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_abnum.py \
     --num_tasks 10 \
@@ -12,7 +43,7 @@ for lambda in 1 3 5 20 50 100 200; do
     --epochs 50 \
     --num_sample $lambda \
     --perfix $lambda- \
-    --cpn_initial means &
+    --cpn_initial means
 
 
   CUDA_VISIBLE_DEVICES=0 python main_imagenet100_v8_abnum.py \
