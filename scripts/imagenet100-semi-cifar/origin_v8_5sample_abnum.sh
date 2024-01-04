7ffdf19dcfde04f0c026c5a462a26553ed6eb23f
lwz <905328134@qq.com>
2024-01-05 00:39:16 +0800

    initial

--- scripts/imagenet100-semi-cifar/origin_v8_5sample_abnum.sh
+++ scripts/imagenet100-semi-cifar/origin_v8_5sample_abnum.sh
@@ -1,6 +1,38 @@
-for lambda in 1 3 10 20 50 100 200; do
+#for lambda in 1 3 10 20 50 100 200; do
+#
+#    CUDA_VISIBLE_DEVICES=6 python main_cifar100_origin_v8_s5_abnum.py \
+#    --num_tasks 10 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --dataset cifar100 \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#    --pl_lambda 0.02 \
+#    --project semi-IPC-abnum \
+#    --epochs 50 \
+#    --num_sample $lambda \
+#    --perfix $lambda- \
+#    --cpn_initial means &
+#
+#  CUDA_VISIBLE_DEVICES=6 python main_cifar100_origin_v8_s5_abnum.py \
+#    --num_tasks 5 \
+#    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#    --pretrained_method byol \
+#    --dataset cifar100 \
+#    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#    --pl_lambda 0.02 \
+#    --project semi-IPC-abnum \
+#    --epochs 50 \
+#    --num_sample $lambda \
+#    --perfix $lambda- \
+#    --cpn_initial means
+#
+#done
 
-    CUDA_VISIBLE_DEVICES=6 python main_cifar100_origin_v8_s5_abnum.py \
+
+
+for lambda in 500; do
+
+    CUDA_VISIBLE_DEVICES=3 python main_cifar100_origin_v8_s5_abnum.py \
     --num_tasks 10 \
     --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
     --pretrained_method byol \
@@ -11,9 +43,9 @@ for lambda in 1 3 10 20 50 100 200; do
     --epochs 50 \
     --num_sample $lambda \
     --perfix $lambda- \
-    --cpn_initial means &
+    --cpn_initial means
 
-  CUDA_VISIBLE_DEVICES=6 python main_cifar100_origin_v8_s5_abnum.py \
+  CUDA_VISIBLE_DEVICES=3 python main_cifar100_origin_v8_s5_abnum.py \
     --num_tasks 5 \
     --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
     --pretrained_method byol \
