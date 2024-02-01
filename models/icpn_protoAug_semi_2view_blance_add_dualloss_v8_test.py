417c323888acfbd30074e4815014e923ceb7aff0
lwz <905328134@qq.com>
2024-02-01 19:12:19 +0800

    initial

--- models/icpn_protoAug_semi_2view_blance_add_dualloss_v8_test.py
+++ models/icpn_protoAug_semi_2view_blance_add_dualloss_v8_test.py
@@ -209,12 +209,16 @@ class IncrementalCPN(pl.LightningModule):
         # 指定的Epoch输出表格
         if (self.current_epoch + 1) % self.epochs == 0:
             self.acc_list.append(avg_acc.item())  # 更新平均准确率列表
-            print("\nMetric | Value")
-            print("-" * 20)
-            print(f"Task acc | {avg_acc.item():12.4f}")
+            print("\nMetric    | Value")
+            print("-" * 3)
+            print(f"Task Acc | {avg_acc.item():12.4f}")
+            print(f"Avg  Acc | {avg_acc.item():12.4f}")
+            print(f"PD       | {avg_acc.item():12.4f}")
+            print(f"CER      | {avg_acc.item():12.4f}")
+            print(f"LSRR     | {avg_acc.item():12.4f}")
             # for epoch, acc in enumerate(self.acc_list, 1):
             #     print(f"{epoch:5d} | {acc:12.4f}")
-            print("-" * 20)
+            print("-" * 30)
 
     def protoAug_start(self):
         # self.radius = 0.1
