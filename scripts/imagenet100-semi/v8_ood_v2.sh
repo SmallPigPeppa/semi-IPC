674eaea536372836c863c51f5f231f48f0dc501a
lwz <905328134@qq.com>
2024-03-12 22:04:39 +0800

    initial

--- scripts/imagenet100-semi/v8_ood_v2.sh
+++ scripts/imagenet100-semi/v8_ood_v2.sh
@@ -24,18 +24,61 @@
 #  --data_path_ood /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet-subset-new
 
 
-for n in 65 130 260  520; do
-  CUDA_VISIBLE_DEVICES=1 python main_imagenet100_v8_ood.py \
-    --num_tasks 5 \
-    --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
-    --pretrained_method byol \
-    --dataset imagenet100 \
-    --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
-    --pl_lambda 0.2 \
-    --project semi-IPC-ood \
-    --epochs 50 \
-    --perfix v8-semi-dual- \
-    --cpn_initial means \
-    --num_ood $n \
-    --data_path_ood /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet-subset-new
-done
+#for n in 65 130 260  520; do
+CUDA_VISIBLE_DEVICES=1 python main_imagenet100_v8_ood.py \
+  --num_tasks 5 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --project semi-IPC-ood \
+  --epochs 50 \
+  --perfix v8-semi-dual- \
+  --cpn_initial means \
+  --num_ood 65 \
+  --data_path_ood /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet-subset-new &
+
+
+CUDA_VISIBLE_DEVICES=2 python main_imagenet100_v8_ood.py \
+  --num_tasks 5 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --pl_lambda 0.2 \
+  --project semi-IPC-ood \
+  --epochs 50 \
+  --perfix v8-semi-dual- \
+  --cpn_initial means \
+  --num_ood 130 \
+  --data_path_ood /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet-subset-new &
+
+CUDA_VISIBLE_DEVICES=3 python main_imagenet100_v8_ood.py \
+--num_tasks 5 \
+--pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+--pretrained_method byol \
+--dataset imagenet100 \
+--data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+--pl_lambda 0.2 \
+--project semi-IPC-ood \
+--epochs 50 \
+--perfix v8-semi-dual- \
+--cpn_initial means \
+--num_ood 260 \
+--data_path_ood /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet-subset-new &
+
+
+CUDA_VISIBLE_DEVICES=4 python main_imagenet100_v8_ood.py \
+--num_tasks 5 \
+--pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+--pretrained_method byol \
+--dataset imagenet100 \
+--data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+--pl_lambda 0.2 \
+--project semi-IPC-ood \
+--epochs 50 \
+--perfix v8-semi-dual- \
+--cpn_initial means \
+--num_ood 520 \
+--data_path_ood /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet-subset-new
