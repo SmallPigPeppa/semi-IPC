439f314df6e511892777c7a838c139ae7c965cdb
lwz <905328134@qq.com>
2024-03-20 16:12:35 +0800

    initial

--- main_upbound.sh
+++ main_upbound.sh
@@ -8,11 +8,43 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python main_upbound.py \
   --project semi-IPC-upbound \
   --epochs 50 \
   --num_gpus 4 \
+  --batch_size 256 &
+
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python main_upbound.py \
+  --num_tasks 1 \
+  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/3tx0at58/byol-imagenet-3tx0at58-ep=999.ckpt \
+  --pretrained_method byol \
+  --dataset imagenet100 \
+  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/imagenet100 \
+  --project semi-IPC-upbound \
+  --epochs 50 \
+  --num_gpus 4 \
   --batch_size 256
 
-
-
-
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python main_upbound.py \
+#  --num_tasks 1 \
+#  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/solo-learn-latest/trained_models/byol/frxj6kgh/byol-imagenet-mini-frxj6kgh-ep=999.ckpt \
+#  --pretrained_method byol \
+#  --dataset mini \
+#  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/miniImageNet \
+#  --project semi-IPC-upbound \
+#  --epochs 50 \
+#  --num_gpus 4 \
+#  --batch_size 256
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python main_upbound.py \
+#  --num_classes 200 \
+#  --num_tasks 1 \
+#  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/test-code/semi-IPC-v9/byol-cub100-resnet50.ckpt \
+#  --pretrained_method byol \
+#  --dataset cub200 \
+#  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds/cub200 \
+#  --project semi-IPC-upbound \
+#  --epochs 50 \
+#  --num_gpus 4 \
+  --batch_size 256
 
 
 
