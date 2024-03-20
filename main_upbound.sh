c6043db88037f1e9de4ae4366dde0d234780431f
lwz <905328134@qq.com>
2024-03-20 15:58:54 +0800

    initial

--- main_upbound.sh
+++ main_upbound.sh
@@ -1,5 +1,5 @@
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python main_v8_upboud.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main_upboud.py \
   --num_tasks 1 \
   --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
   --pretrained_method byol \
