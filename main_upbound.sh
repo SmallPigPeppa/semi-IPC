a7716f789aec0bf9dd0b0d988d1dba06470cf1a4
lwz <905328134@qq.com>
2024-03-20 16:17:36 +0800

    initial

--- main_upbound.sh
+++ main_upbound.sh
@@ -1,14 +1,14 @@
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python main_upbound.py \
-  --num_tasks 1 \
-  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
-  --pretrained_method byol \
-  --dataset cifar100 \
-  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
-  --project semi-IPC-upbound \
-  --epochs 50 \
-  --num_gpus 4 \
-  --batch_size 256 &
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python main_upbound.py \
+#  --num_tasks 1 \
+#  --pretrained_model /mnt/mmtech01/usr/liuwenzhuo/code/solo-learn/trained_models/byol/t3pmk238/byol-imagenet32-t3pmk238-ep=999.ckpt \
+#  --pretrained_method byol \
+#  --dataset cifar100 \
+#  --data_path /mnt/mmtech01/usr/liuwenzhuo/torch_ds \
+#  --project semi-IPC-upbound \
+#  --epochs 50 \
+#  --num_gpus 4 \
+#  --batch_size 256 &
 
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python main_upbound.py \
